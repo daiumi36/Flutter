e095d89a8455080b110d7607bb1813d3d7b771f5
Nguyễn Văn Đại <daian982003@gmail.com>
2024-09-19 13:49:51 +0700

    Update function.dart

--- Ex/EX_Function2/function.dart
+++ Ex/EX_Function2/function.dart
@@ -1,4 +1,4 @@
-void hoanDoi(List<dynamic> mang, int index1, int index2) {
+/*void hoanDoi(List<dynamic> mang, int index1, int index2) {
   if (index1 < 0 ||
       index1 >= mang.length ||
       index2 < 0 ||
@@ -19,3 +19,49 @@ void main() {
   hoanDoi(mang, 1, 3);
   print("Mảng sau khi hoán đổi: $mang");
 }
+*/
+/*
+class Queue<T> {
+  List<T> _elements = [];
+
+  void enqueue(T value) {
+    _elements.add(value);
+  }
+
+  T? dequeue() {
+    if (_elements.isEmpty) {
+      print("Queue trống");
+      return null;
+    }
+    return _elements.removeAt(0);
+  }
+
+  bool isEmpty() {
+    return _elements.isEmpty;
+  }
+
+  void printQueue() {
+    if (_elements.isEmpty) {
+      print("Queue trống");
+    } else {
+      print("Queue hiện tại: $_elements");
+    }
+  }
+}
+
+void main() {
+  Queue<int> queue = Queue<int>();
+
+  queue.enqueue(10);
+  queue.enqueue(20);
+  queue.enqueue(30);
+  queue.printQueue(); 
+
+  print("Phần tử bị xóa: ${queue.dequeue()}"); 
+  queue.printQueue(); 
+
+  print("Phần tử bị xóa: ${queue.dequeue()}");
+  queue.printQueue(); 
+}
+*/
+
