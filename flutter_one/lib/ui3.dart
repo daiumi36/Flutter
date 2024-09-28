60ef9cba7dbee6123c6f8ac677e48432eaf7afce
Nguyễn Văn Đại <daian982003@gmail.com>
2024-09-28 16:59:07 +0700

    ui3

--- flutter_one/lib/ui3.dart
+++ flutter_one/lib/ui3.dart
@@ -30,14 +30,14 @@ class VietnamFlag extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 360, // Kích thước lá cờ
+      width: 360, 
       height: 200,
-      color: Colors.red, // Nền đỏ
+      color: Colors.red,
       child: const Center(
         child: Icon(
-          Icons.star, // Icon ngôi sao
-          color: Colors.yellow, // Màu vàng của ngôi sao
-          size: 150, // Kích thước ngôi sao
+          Icons.star, 
+          color: Colors.yellow, 
+          size: 150,
         ),
       ),
     );
